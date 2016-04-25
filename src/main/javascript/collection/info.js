6fd3c0c6f55a1da6b9d91cb38cab796de7f76a44
Alexander Parkhomenko <paraleksi@gmail.com>
2016-04-25 21:55:29 +0300

    Some JS test

--- src/main/javascript/collection/info.js
+++ src/main/javascript/collection/info.js
@@ -1,10 +1,11 @@
-function initPage(statByCategories, statByCountries) {
+function initPage(statByCategories, statByCountries, seriesOfCollection) {
 	var chartsVersion = '44';
 	google.charts.load(chartsVersion, {'packages':['corechart']});
 	google.charts.setOnLoadCallback(function drawCharts() {
 		drawChart('categories-chart', createDataTable(statByCategories));
 		drawChart('countries-chart', createDataTable(statByCountries));
 	});
+	makeCategoriesList('seriesList',seriesOfCollection);
 }
 
 function drawChart(containerId, dataTable) {
@@ -16,6 +17,30 @@ function drawChart(containerId, dataTable) {
 };
 
 function createDataTable(stat) {
+	var table = new google.visualization.DataTable();
+	table.addColumn('string', 'Category/Country');
+	table.addColumn('number', 'Quantity of stamps');
+	table.addRows(stat);
+	return table;
+};
+
+function makeCategoriesList(elementId, categoriesList) {
+	var element = document.getElementById(elementId);
+	element.innerHTML = '';
+	element.innerHTML = 'It a text before list';
+	var list = document.createElement("ul");
+	for(var i in categoriesList) {
+	var elemText = document.createElement("a");
+	elemText.innerHTML = categoriesList[i].category.name;
+	element.appendChild(elemText);
+	
+	var elem = document.createElement("li");
+	elem.appendChild(elemText);
+	list.appendChild(elem);
+	element.appendChild(list);
+	}
+	
+	
 	var table = new google.visualization.DataTable();
 	table.addColumn('string', 'Category/Country');
 	table.addColumn('number', 'Quantity of stamps');
