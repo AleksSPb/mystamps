183f69e7448e1e578f52beb224e9d56475023405
Alexander Parkhomenko <paraleksi@gmail.com>
2016-05-06 15:34:43 +0300

    Make sublist on collection page. Test file for comfort testing, need to delete after review.

--- src/main/javascript/collection/info.js
+++ src/main/javascript/collection/info.js
@@ -1,11 +1,11 @@
 function initPage(statByCategories, statByCountries, seriesOfCollection) {
+	makeGroupedCollectionsList('seriesList',seriesOfCollection, 'category', 'country');
 	var chartsVersion = '44';
 	google.charts.load(chartsVersion, {'packages':['corechart']});
 	google.charts.setOnLoadCallback(function drawCharts() {
 		drawChart('categories-chart', createDataTable(statByCategories));
 		drawChart('countries-chart', createDataTable(statByCountries));
 	});
-	makeCategoriesList('seriesList',seriesOfCollection, 'category.name', 'country.name');
 }
 
 function drawChart(containerId, dataTable) {
@@ -24,32 +24,50 @@ function createDataTable(stat) {
 	return table;
 };
 
-function makeCategoriesList(elementId, categoriesList, firstGrouping, secondGrouping) {
-	var element = document.getElementById(elementId);
-	element.innerHTML = 'It a text before ist';
-	var list = document.createElement("ul");
-	var level1_group = _.groupBy(categoriesList, firstGrouping);
-	console.log(level1_group);
-	console.log('End grouped object')
-	console.log(Object.keys(level1_group)); 
-	
-	console.log('Try to print names')
-	for(var j in level1_group) {
-	console.log(j);
-	console.log(level1_group[j]);
+function makeItemsSublist(array) {
+	var list = document.createElement('ul');
+	for(i in array) {
+		var item = document.createElement('li');
+		// Set its contents:
+		var linke = document.createElement("a");
+				linke.href = '/series/'+ array[i].id;
+				if (array[i].releaseYear) {
+					linke.innerHTML = array[i].releaseYear + ', ' + array[i].quantity + ' item(s)';
+				} else {
+				linke.innerHTML = array[i].quantity + ' item(s)';
+				}
+		item.appendChild(linke);
+		list.appendChild(item);
 	}
-	
+	return list;
+}
 
-	console.log('End of objects')
-	for(var i in categoriesList) {
-	var elemText = document.createElement("a");
-	elemText.href = '/category/' + categoriesList[i].category.id+ '/' + categoriesList[i].category.slug;
-	elemText.innerHTML = categoriesList[i].category.name;
-	element.appendChild(elemText);
-	
-	var elem = document.createElement("li");
-	elem.appendChild(elemText);
-	list.appendChild(elem);
-	element.appendChild(list);
+function makeSublist(array, groupping) {
+	var list = document.createElement('ul');
+	for(i in array) {
+		var item = document.createElement('li');
+		// Set its contents:
+		var linke = document.createElement("a");
+				linke.href = '/'+groupping+'/' + array[i][0][groupping].id+ '/' + array[i][0][groupping].slug;
+				linke.innerHTML = array[i][0][groupping].name;
+		item.appendChild(linke);
+		item.appendChild(makeItemsSublist(array[i]));
+		list.appendChild(item);
+	}
+	return list;
+}
+
+function makeGroupedCollectionsList(elementId, collectioinsList, firstGrouping, secondGrouping) {
+	var parentElement = document.getElementById(elementId);
+	var list = document.createElement("ul");
+	var level1_group = _.groupBy(_.sortBy(collectioinsList,[firstGrouping + '.name', secondGrouping + '.name', 'releaseYear']), firstGrouping + '.name');
+	for (key in level1_group) {
+		var item = document.createElement('li');
+		var elemText = document.createElement("a");
+		elemText.href = '/'+firstGrouping+'/' + level1_group[key][0][firstGrouping].id+ '/' + level1_group[key][0][firstGrouping].slug;
+		elemText.innerHTML = level1_group[key][0][firstGrouping].name;
+		item.appendChild(elemText);
+		item.appendChild(makeSublist(_.groupBy(level1_group[key], secondGrouping + '.name'), secondGrouping))
+		parentElement.appendChild(item);
 	}
 };
--- src/main/javascript/collection/test_info.js
+++ src/main/javascript/collection/test_info.js
@@ -1,56 +1,60 @@
-function initPage(seriesOfCollection) {
+function initPage1(seriesOfCollection) {
 
-	makeCategoriesList('seriesList',seriesOfCollection, 'category', 'country');
+	makeGroupedCollectionsList1('seriesList',seriesOfCollection, 'category', 'country');
 }
 
-function makeUL(array, groupping, name, slugfield) {
-console.log(array);
-    // Create the list element:
+function makeItemsSublist1(array) {
     var list = document.createElement('ul');
-
     for(i in array) {
-        // Create the list item:
-        console.log("Name:" + i);
-        console.log(array[i]);
         var item = document.createElement('li');
+        // Set its contents:
+        var linke = document.createElement("a");
+        		linke.href = '/series/'+ array[i].id;
+        		if (array[i].releaseYear) {
+        			linke.innerHTML = array[i].releaseYear + ', ' + array[i].quantity + ' item(s)';
+        		} else {
+        		linke.innerHTML = array[i].quantity + ' item(s)';
+        		}
+        item.appendChild(linke);
+        list.appendChild(item);
+    }
+    return list;
+}
 
+function makeSublist1(array, groupping) {
+    var list = document.createElement('ul');
+    for(i in array) {
+        var item = document.createElement('li');
         // Set its contents:
         var linke = document.createElement("a");
         		linke.href = '/'+groupping+'/' + array[i][0][groupping].id+ '/' + array[i][0][groupping].slug;
         		linke.innerHTML = array[i][0][groupping].name;
         item.appendChild(linke);
-        // Add it to the list:
+        item.appendChild(makeItemsSublist1(array[i]));
         list.appendChild(item);
     }
-
-    // Finally, return the constructed list:
     return list;
 }
 
-function makeCategoriesList(elementId, categoriesList, firstGrouping, secondGrouping) {
+function makeGroupedCollectionsList1(elementId, collectioinsList, firstGrouping, secondGrouping) {
 	var parentElement = document.getElementById(elementId);
-	parentElement.innerHTML = 'It a text before list';
 	var list = document.createElement("ul");
-	var level1_group = _.groupBy(_.sortBy(categoriesList,firstGrouping + '.name'), firstGrouping + '.name');
+	var level1_group = _.groupBy(_.sortBy(collectioinsList,[firstGrouping + '.name', secondGrouping + '.name', 'releaseYear']), firstGrouping + '.name');
 	
 	console.log('Try to print names')
 	for (key in level1_group) {
 		console.log(key);
 		console.log(level1_group[key]);
 	}
-	
 	console.log('End of objects');
 	
-	
 	for (key in level1_group) {
-	var item = document.createElement('li');
+		var item = document.createElement('li');
 		var elemText = document.createElement("a");
-		elemText.href = '/'+firstGrouping+'/' + level1_group[key][0].category.id+ '/' + level1_group[key][0].category.slug;
-		elemText.innerHTML = level1_group[key][0].category.name;
+		elemText.href = '/'+firstGrouping+'/' + level1_group[key][0][firstGrouping].id+ '/' + level1_group[key][0][firstGrouping].slug;
+		elemText.innerHTML = level1_group[key][0][firstGrouping].name;
 		item.appendChild(elemText);
-		var level2_group = _.groupBy(_.sortBy(level1_group[key], secondGrouping + '.name'),secondGrouping + '.name');
-		item.appendChild(makeUL(level2_group, 'country', 'name', 'slug'))
+		item.appendChild(makeSublist1(_.groupBy(level1_group[key], secondGrouping + '.name'), secondGrouping))
 		parentElement.appendChild(item);
-		
 	}
 };
